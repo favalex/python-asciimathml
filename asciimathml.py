80b7bddd4f1df811f4ed0103baaa1da2b74eb21a
Gabriele Favalessa <gabriele@develer.com>
2010-07-18 12:22:17 +0200

    Some unary an binary operators. subsup.

--- asciimathml.py
+++ asciimathml.py
@@ -32,7 +32,7 @@ def parse(s):
     ns = []
 
     while True:
-        n, s = parse_(s)
+        n, s, i = parse_(s)
         if not n is None:
             ns.append(n)
 
@@ -42,41 +42,101 @@ def parse(s):
                 ns = ns[:-3]
                 ns.append(n)
             elif is_sub(ns[-2]):
-                n = El('msub', ns[-3], ns[-1])
+                # TODO use under over if ns[-3] requires it
+                if ns[-3].tag == 'msup':
+                    children = ns[-3].getchildren()
+                    n = El('msubsup', children[0], ns[-1], children[1])
+                else:
+                    n = El('msub', ns[-3], ns[-1])
                 ns = ns[:-3]
                 ns.append(n)
             elif is_sup(ns[-2]):
-                n = El('msup', ns[-3], ns[-1])
+                # TODO use under over if ns[-3] requires it
+                if ns[-3].tag == 'msub':
+                    children = ns[-3].getchildren()
+                    n = El('msubsup', children[0], children[1], ns[-1])
+                else:
+                    n = El('msup', ns[-3], ns[-1])
                 ns = ns[:-3]
                 ns.append(n)
 
+        if i.type == BINARY:
+            a, s, i = parse_(s)
+            b, s, i = parse_(s)
+            n = ns[-1]
+            ns[-1] = El(n.tag, a, b)
+        elif i.type == UNARY:
+            a, s, ai = parse_(s)
+            n = ns[-1]
+            if n.text is None:
+                ns[-1] = El(n.tag, a)
+            else:
+                # mo is hardcoded. Good enough?
+                ns[-1] = El(n.tag, El('mo', text=n.text), a)
+
         if s == '':
+            # return El('math', El('mstyle', *ns, attrib={"displaystyle": "true"}), attrib={"xmlns": "http://www.w3.org/1998/Math/MathML"})
             return El('math', El('mstyle', *ns))
 
 def parse_(s):
     s = s.strip()
 
     if s == '':
-        return None, ''
+        return None, '', SymbolInfo(type=CONST)
 
     m = number_re.match(s)
 
     if m:
-        return El('mn', text=m.group(0)), s[m.end():]
+        return El('mn', text=m.group(0)), s[m.end():], SymbolInfo(type=CONST)
 
     for y in symbols:
         if s.startswith(y.input):
-            return El(y.tag, text=y.output), s[len(y.input):]
+            return El(y.tag, text=y.output), s[len(y.input):], SymbolInfo(type=y.type)
 
-    return El('mi', text=s[0]), s[1:]
+    return El('mi', text=s[0]), s[1:], SymbolInfo(type=CONST)
 
-Symbol = namedtuple('Symbol', 'input tag output tex type')
-CONST = 1
+Symbol = namedtuple('Symbol', 'input tag output type')
+# TODO arity=0,1,2
+# TODO subsup=SUBSUP,UNDEROVER
+
+SymbolInfo = namedtuple('SymbolInfo', 'type')
+# TODO private element attribs '_type'
+
+CONST = 1; BINARY = 2; UNARY = 3
 
 symbols = [
-    Symbol(input="alpha",  tag="mi", output=u"\u03B1", tex=None, type=CONST),
+    Symbol(input="alpha",  tag="mi", output=u"\u03B1", type=CONST),
+    Symbol(input="beta",  tag="mi", output=u"\u03B2", type=CONST),
+    Symbol(input="gamma",  tag="mi", output=u"\u03B3", type=CONST),
+
+    Symbol(input="*",  tag="mo", output="\u22C5", type=CONST),
+    Symbol(input="**", tag="mo", output="\u22C6", type=CONST),
+
+    Symbol(input="sum", tag="mo", output="\u2211", type=CONST), # type=UNDEROVER),
+
+    Symbol(input="sin", tag="mrow", output="sin", type=UNARY), # TODO output=El('mo', text='sin')
+    Symbol(input="dot", tag="mover", output=".", type=UNARY),
+    Symbol(input="sqrt", tag="msqrt", output=None, type=UNARY),
+    Symbol(input="text", tag="mtext", output=None, type=UNARY),
+
+    Symbol(input="frac", tag="mfrac", output=None, type=BINARY),
+    Symbol(input="root", tag="mroot", output=None, type=BINARY),
+    Symbol(input="stackrel", tag="mover", output=None, type=BINARY),
 ]
 
 if __name__ == '__main__':
     import sys
+    print """\
+<?xml version="1.0"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="application/xhtml+xml" />
+        <title>ASCIIMathML preview</title>
+    </head>
+    <body>
+"""
     print tostring(parse(' '.join(sys.argv[1:])))
+    print """\
+    </body>
+</html>
+"""
--- test.py
+++ test.py
@@ -18,28 +18,85 @@ class ParseTestCase(unittest.TestCase):
         self.assertTreeEquals(parse('alpha'), El('math', El('mstyle', El('mi', text=u'\u03b1'))))
 
     def testSymbols(self):
-        self.assertTreeEquals(parse('alpha alpha'), El('math', El('mstyle', El('mi', text=u'\u03b1'), El('mi', text=u'\u03b1'))))
+        self.assertTreeEquals(parse('alpha beta'), El('math', El('mstyle', El('mi', text=u'\u03b1'), El('mi', text=u'\u03b2'))))
 
     def testFrac(self):
-        self.assertTreeEquals(parse('alpha / alpha'),
+        self.assertTreeEquals(parse('alpha / beta'),
             El('math', El('mstyle',
                 El('mfrac',
                     El('mi', text=u'\u03b1'),
-                    El('mi', text=u'\u03b1')))))
+                    El('mi', text=u'\u03b2')))))
 
     def testSub(self):
-        self.assertTreeEquals(parse('alpha _ alpha'),
+        self.assertTreeEquals(parse('alpha _ beta'),
             El('math', El('mstyle',
                 El('msub',
                     El('mi', text=u'\u03b1'),
-                    El('mi', text=u'\u03b1')))))
+                    El('mi', text=u'\u03b2')))))
 
     def testSup(self):
-        self.assertTreeEquals(parse('alpha ^ alpha'),
+        self.assertTreeEquals(parse('alpha ^ beta'),
             El('math', El('mstyle',
                 El('msup',
                     El('mi', text=u'\u03b1'),
+                    El('mi', text=u'\u03b2')))))
+
+    def testSubSup(self):
+        self.assertTreeEquals(parse('alpha _ beta ^ gamma'),
+            El('math', El('mstyle',
+                El('msubsup',
+                    El('mi', text=u'\u03b1'),
+                    El('mi', text=u'\u03b2'),
+                    El('mi', text=u'\u03b3')))))
+
+    def testSupSub(self):
+        self.assertTreeEquals(parse('alpha ^ beta _ gamma'),
+            El('math', El('mstyle',
+                El('msubsup',
+                    El('mi', text=u'\u03b1'),
+                    El('mi', text=u'\u03b3'),
+                    El('mi', text=u'\u03b2')))))
+
+    def testUnary(self):
+        self.assertTreeEquals(parse('sin alpha'),
+            El('math', El('mstyle',
+                El('mrow',
+                    El('mo', text='sin'),
                     El('mi', text=u'\u03b1')))))
+
+    def testUnary2(self):
+        self.assertTreeEquals(parse('dot alpha'),
+            El('math', El('mstyle',
+                El('mover',
+                    El('mo', text='.'),
+                    El('mi', text=u'\u03b1')))))
+
+    def testUnary3(self):
+        self.assertTreeEquals(parse('sqrt alpha'),
+            El('math', El('mstyle',
+                El('msqrt',
+                    El('mi', text=u'\u03b1')))))
+
+    def testUnary4(self):
+        self.assertTreeEquals(parse('text alpha'),
+            El('math', El('mstyle',
+                El('mtext',
+                    El('mi', text=u'\u03b1')))))
+
+    def testBinary(self):
+        self.assertTreeEquals(parse('frac alpha beta'),
+            El('math', El('mstyle',
+                El('mfrac',
+                    El('mi', text=u'\u03b1'),
+                    El('mi', text=u'\u03b2')))))
+
+    def testUnderOver(self):
+        self.assertTreeEquals(parse('sum_alpha^beta'),
+            El('math', El('mstyle',
+                El('munderover',
+                    El('mo', text=u'\u2211'),
+                    El('mi', text=u'\u03b1'),
+                    El('mi', text=u'\u03b2')))))
 
 if __name__ == '__main__':
     unittest.main()
